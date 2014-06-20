1e5008f1522db79a955b578604a76a67dcfe3ce0
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-06-20 17:44:26 +0200

    Nice.

--- Clippy/Clippy/GetText.vb
+++ Clippy/Clippy/GetText.vb
@@ -3,4 +3,8 @@
     Private Sub GetTextFromClipboard_Tick(sender As Object, e As EventArgs) Handles GetTextFromClipboard.Tick
         RichTextBox1.Text = Clipboard.GetText()
     End Sub
+
+    Private Sub GetText_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetTextFromClipboard.Start()
+    End Sub
 End Class
