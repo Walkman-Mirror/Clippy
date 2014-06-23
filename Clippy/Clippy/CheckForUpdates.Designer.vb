de2b222924400b603563c4890485eeb66755c80c
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-06-23 18:16:09 +0200

    Continueing work on the update checker.

--- Clippy/Clippy/CheckForUpdates.Designer.vb
+++ Clippy/Clippy/CheckForUpdates.Designer.vb
@@ -26,6 +26,8 @@ Partial Class CheckForUpdates
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -51,16 +53,39 @@ Partial Class CheckForUpdates
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(106, 59)
+        Me.ProgressBar1.Location = New System.Drawing.Point(106, 49)
         Me.ProgressBar1.Name = "ProgressBar1"
         Me.ProgressBar1.Size = New System.Drawing.Size(199, 23)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee
         Me.ProgressBar1.TabIndex = 4
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(106, 84)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.TabIndex = 5
+        Me.WebBrowser1.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(211, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 28)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "&Get update"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
         '
         'CheckForUpdates
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(358, 116)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.WebBrowser1)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
@@ -80,4 +105,6 @@ Partial Class CheckForUpdates
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
--- Clippy/Clippy/CheckForUpdates.vb
+++ Clippy/Clippy/CheckForUpdates.vb
@@ -1,3 +1,20 @@
 ﻿Public Class CheckForUpdates
 
+    Private Sub CheckForUpdates_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Check()
+    End Sub
+
+    Sub Check()
+        WebBrowser1.Navigate("https://deavmi.github.io/Clippy/current_version.txt")
+    End Sub
+
+    Private Sub ReadData()
+        If WebBrowser1.Document.Body.InnerText > My.Application.Info.Version.ToString Then
+            Label1.Text = "An update is available."
+            btnGetUpdate.Visible = False
+        Else
+            Label1.Text = "No updates available"
+            btnGetUpdate.Visible = False
+        End If
+    End Sub
 End Class
